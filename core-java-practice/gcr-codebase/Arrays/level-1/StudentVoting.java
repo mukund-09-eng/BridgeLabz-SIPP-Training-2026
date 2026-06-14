84b5cf19b650034ac2e007269923090117831780
Mukund Bansal <mukundbansal0001@gmail.com>
2026-06-14 14:59:10 +0530

    Added level 3 control flow programs

---
+++ core-java-practice/gcr-codebase/Arrays/level-1/StudentVoting.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StudentVoting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] ages = new int[10];
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+        for (int age : ages) {
+            if (age < 0) {
+                System.out.println("Invalid Age");
+            } else if (age >= 18) {
+                System.out.println("The student with age " + age + " can vote.");
+            } else {
+                System.out.println("The student with age " + age + " cannot vote.");
+            }
+        }
+    }
+}
