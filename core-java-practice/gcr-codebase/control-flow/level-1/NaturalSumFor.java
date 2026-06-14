04ab770a86e5dcc4d008a77e7223d7b9fbb05876
Mukund Bansal <mukundbansal0001@gmail.com>
2026-06-14 14:18:34 +0530

    Added control flow level 1 Java programs

---
+++ core-java-practice/gcr-codebase/control-flow/level-1/NaturalSumFor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NaturalSumFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+
+            int formulaSum = n * (n + 1) / 2;
+
+            System.out.println("Sum using For Loop = " + sum);
+            System.out.println("Sum using Formula = " + formulaSum);
+            System.out.println("Both results are equal? " + (sum == formulaSum));
+        } else {
+            System.out.println("Not a Natural Number");
+        }
+    }
+}
---
+++ core-java-practice/gcr-codebase/control-flow/level-1/SumWithBreak.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumWithBreak {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0.0;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            double number = sc.nextDouble();
+
+            if (number <= 0) {
+                break;
+            }
+
+            total += number;
+        }
+
+        System.out.println("Total Sum = " + total);
+    }
+}
