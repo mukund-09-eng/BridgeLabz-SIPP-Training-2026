7c8d65d3f3a2d429903484079c1a9536ca4963ae
Mukund Bansal <mukundbansal0001@gmail.com>
2026-06-14 14:47:36 +0530

    Added level 3 control flow programs

---
+++ core-java-practice/gcr-codebase/control-flow/level-3/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double first = sc.nextDouble();
+        String op = sc.next();
+        double second = sc.nextDouble();
+
+        switch (op) {
+            case "+":
+                System.out.println(first + second);
+                break;
+
+            case "-":
+                System.out.println(first - second);
+                break;
+
+            case "*":
+                System.out.println(first * second);
+                break;
+
+            case "/":
+                System.out.println(first / second);
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
---
+++ core-java-practice/gcr-codebase/control-flow/level-3/GradeCalculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double physics = sc.nextDouble();
+        double chemistry = sc.nextDouble();
+        double maths = sc.nextDouble();
+
+        double average = (physics + chemistry + maths) / 3;
+        String grade, remarks;
+
+        if (average >= 80) {
+            grade = "A";
+            remarks = "Level 4";
+        } else if (average >= 70) {
+            grade = "B";
+            remarks = "Level 3";
+        } else if (average >= 60) {
+            grade = "C";
+            remarks = "Level 2";
+        } else if (average >= 50) {
+            grade = "D";
+            remarks = "Level 1";
+        } else if (average >= 40) {
+            grade = "E";
+            remarks = "Level 1-";
+        } else {
+            grade = "R";
+            remarks = "Remedial Standards";
+        }
+
+        System.out.println("Average = " + average);
+        System.out.println("Grade = " + grade);
+        System.out.println("Remarks = " + remarks);
+    }
+}
