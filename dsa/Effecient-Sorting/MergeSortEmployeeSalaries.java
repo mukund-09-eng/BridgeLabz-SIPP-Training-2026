7fc38d28e0c6c2cf499eb7e72f673c44fd1209b0
Mukund Bansal <mukundbansal0001@gmail.com>
2026-07-07 22:39:24 +0530

    [Mukund]:Effecient Sorting Done

---
+++ dsa/Effecient-Sorting/MergeSortEmployeeSalaries.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class MergeSortEmployeeSalaries {
+
+    static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] salaries = {45000, 32000, 78000, 55000, 40000, 60000};
+
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(salaries));
+
+        mergeSort(salaries, 0, salaries.length - 1);
+
+        System.out.println("After Sorting:");
+        System.out.println(Arrays.toString(salaries));
+    }
+}
