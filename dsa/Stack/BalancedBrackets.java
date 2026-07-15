056f63b82376d444edea11df70f908a1a5bbc266
Mukund Bansal <mukundbansal0001@gmail.com>
2026-07-15 14:29:21 +0530

    [Mukund]: Stack Questions Solved

---
+++ dsa/Stack/BalancedBrackets.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class BalancedBrackets {
+
+    public static boolean isValidConfig(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(')
+                    return false;
+
+                if (ch == '}' && top != '{')
+                    return false;
+
+                if (ch == ']' && top != '[')
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "{[()]}";
+
+        if (isValidConfig(s))
+            System.out.println("Valid");
+        else
+            System.out.println("Invalid");
+    }
+}
