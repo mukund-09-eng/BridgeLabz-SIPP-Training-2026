2c3d94cc5f869e637d3e8cfcc6e7becdea9091ac
Mukund Bansal <mukundbansal0001@gmail.com>
2026-07-21 09:26:56 +0530

    [MUKUND] Hashing Assignment done

---
+++ dsa/Hashing/SubarraySumEqualsK.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+
+    public static int subarraySumEqualsK(int[] revenueChanges, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1);
+
+        int runningSum = 0;
+        int answer = 0;
+
+        for (int change : revenueChanges) {
+            runningSum += change;
+
+            answer += prefixCount.getOrDefault(runningSum - k, 0);
+
+            prefixCount.merge(runningSum, 1, Integer::sum);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(subarraySumEqualsK(arr, 3));
+    }
+}
